c2a8d8dec7a6dcdfa04c24332563fb2ebd7a8801
krishnaamlgolabs <krishna.singh@amlgolabs.com>
2025-04-08 16:49:08 +0530

    metaTagsAdded

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -5,6 +5,7 @@ import Footer1 from "./components/Footer/Footer1";
 import RebrandingMarquee from "./components/RebrandingMessage";
 import RebrandingMessage from "./components/RebrandingMessage";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +20,19 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Welcome to Amlgo Labs",
   description: "Amlgo Labs empowers businesses with AI, GenAI, Machine Learning, and Data Analytics solutions. Unlock data-driven growth with Amlgo Labs today.",
+  other: {
+    'google-site-verification': '5wCSrDbYEEkN5n80skeTvLxnJB6SYeV1UY4bfehUsOU',
+  },
 };
+
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="5wCSrDbYEEkN5n80skeTvLxnJB6SYeV1UY4bfehUsOU" />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {/* <RebrandingMarquee/> */}
         <Header1 />
