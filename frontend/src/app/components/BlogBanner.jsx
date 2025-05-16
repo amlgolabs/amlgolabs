7586e5aaa58ab76cafda26b35f2999ca3f85555a
chiragamlgo <chirag.bang@amlgolabs.com>
2025-05-16 17:16:46 +0530

    blogBanner

--- frontend/src/app/components/BlogBanner.jsx
+++ frontend/src/app/components/BlogBanner.jsx
@@ -1,3 +1,73 @@
+// import React from "react";
+// import styles from "../styles/pages/Components/BlogBanner.module.css";
+// import PropTypes from "prop-types";
+// import { useRouter } from "next/navigation";
+// import Link from "next/link";
+
+// const BlogBanner = ({
+//   title = "Explore Resources",
+//   subtitle = "Have a question, need support, or curious about how we can elevate your business? Our team is here to collaborate with you.",
+//   containerClass = "",
+//   titleClass = "",
+//   subtitleClass = "",
+//   selectedContentType = "",
+// }) => {
+//   const router = useRouter();
+
+//   const handleContentTypeClick = (contentType) => {
+//     router.push(
+//       `/resources/filter?contentType=${encodeURIComponent(contentType)}`
+//     );
+//   };
+
+//   const isActive = (contentType) => {
+//     return selectedContentType === contentType ? styles.activeButton : "";
+//   };
+
+//   return (
+//     <section className={`${styles.bannerContainer}`}>
+//       <div className={styles.contentWrapper}>
+//         <h5 className={`${styles.title} ${titleClass}`}>
+//           <Link href="/resources">Resources</Link>
+//         </h5>
+//         <div className={styles.buttonGroup}>
+//           <button
+//             className={`globalButton ${isActive("Case Study")}`}
+//             onClick={() => handleContentTypeClick("Case Study")}
+//           >
+//             Case Study
+//           </button>
+//           <button
+//             className={`globalButton ${isActive("Blog")}`}
+//             onClick={() => handleContentTypeClick("Blog")}
+//           >
+//             Blog
+//           </button>
+//           <button
+//             className={`globalButton ${isActive("White Paper")}`}
+//             onClick={() => handleContentTypeClick("White Paper")}
+//           >
+//             White Paper
+//           </button>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// BlogBanner.propTypes = {
+//   title: PropTypes.string,
+//   subtitle: PropTypes.string,
+//   containerClass: PropTypes.string,
+//   titleClass: PropTypes.string,
+//   subtitleClass: PropTypes.string,
+//   selectedContentType: PropTypes.string,
+// };
+
+// export default BlogBanner;
+
+
+
 import React from "react";
 import styles from "../styles/pages/Components/BlogBanner.module.css";
 import PropTypes from "prop-types";
@@ -25,30 +95,22 @@ const BlogBanner = ({
   };
 
   return (
-    <section className={`${styles.bannerContainer}`}>
+    <section className={`${styles.bannerContainer} ${containerClass}`}>
       <div className={styles.contentWrapper}>
         <h5 className={`${styles.title} ${titleClass}`}>
           <Link href="/resources">Resources</Link>
         </h5>
+        {/* {subtitle && <p className={`${styles.subtitle} ${subtitleClass}`}>{subtitle}</p>} */}
         <div className={styles.buttonGroup}>
-          <button
-            className={`globalButton ${isActive("Case Study")}`}
-            onClick={() => handleContentTypeClick("Case Study")}
-          >
-            Case Study
-          </button>
-          <button
-            className={`globalButton ${isActive("Blog")}`}
-            onClick={() => handleContentTypeClick("Blog")}
-          >
-            Blog
-          </button>
-          <button
-            className={`globalButton ${isActive("White Paper")}`}
-            onClick={() => handleContentTypeClick("White Paper")}
-          >
-            White Paper
-          </button>
+          {["Case Study", "Blog", "White Paper"].map((type) => (
+            <button
+              key={type}
+              className={`${styles.navButton} ${isActive(type)}`}
+              onClick={() => handleContentTypeClick(type)}
+            >
+              {type}
+            </button>
+          ))}
         </div>
       </div>
     </section>
